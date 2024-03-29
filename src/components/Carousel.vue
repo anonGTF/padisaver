dda742f1933239a46c6c17ad5ea76ad3edb16463
galih.tegar <galih.tegar.febrianto@bni.co.id>
2024-03-29 09:36:14 +0700

    adjust carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,25 +1,45 @@
 <template>
-  <div class="carousel carousel-center max-w-md my-auto p-4 space-x-4 bg-neutral rounded-box">
+  <div class="overflow-hidden my-auto" ref="emblaRef">
     <div 
-      v-for="hama in items" 
-      :key="hama.name" 
-      class="carousel-item relative w-[320px] h-[480px] hover:cursor-pointer"
-      @click="router.push(`/detail/${toLink(hama.name)}`)"
+      class="flex max-w-md p-4 space-x-4 bg-neutral rounded-box"
+      :class="{'justify-center': scrollSnaps.length == 1}"
     >
-      <img :src="hama.thumbnail" class="rounded-box absolute top-0 left-0 object-cover w-[320px] h-[480px]" />
-      <div class="z-10 absolute bottom-0 left-0 w-full px-3 pb-3 pt-6 rounded-b-box bg-gradient-to-t from-black to-transparent">
-        <p class="text-2xl font-semibold">{{ hama.name }}</p>
-        <p class="italic">{{ hama.scientificName }}</p>
+      <div 
+        v-for="hama in items" 
+        :key="hama.name" 
+        class="flex-[0_0_80%] h-[480px] hover:cursor-pointer shadow-lg"
+        @click="router.push(`/detail/${toLink(hama.name)}`)"
+      >
+        <img :src="hama.thumbnail" class="rounded-t-box object-cover w-[320px] h-[390px]" />
+        <div class="w-full px-3 py-4 rounded-b-box bg-[#E6DEB9] text-[#3B2E1E]">
+          <p class="text-2xl font-semibold">{{ hama.name }}</p>
+          <p class="italic">{{ hama.scientificName }}</p>
+        </div>
       </div>
+    </div>
+    <div v-if="scrollSnaps.length > 1" class="flex flex-wrap items-center justify-center mt-8 gap-2 bg-[#E6DEB9] rounded-full w-fit mx-auto px-3 py-2">
+      <button 
+        v-for="(_, index) in scrollSnaps" 
+        :key="index"
+        @click="onDotButtonClick(index)"
+        class="appearance-none touch-manipulation cursor-pointer w-6 h-6 rounded-full bg-white"
+        :class="{'bg-[#3B2E1E]': selectedIndex == index}"
+      ></button>
     </div>
   </div>
 </template>
 
 <script setup>
+  import emblaCarouselVue from 'embla-carousel-vue'
   import { useRouter } from "vue-router"
   import { toLink } from "../utils/stringUtil";
+  import { useDotButton } from "../composables/useDotButton"
+
+  const [emblaRef, emblaApi] = emblaCarouselVue()
   
   const router = useRouter()
 
   const props = defineProps(['items'])
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi)
 </script>
---
+++ src/composables/useDotButton.js
@@ -0,0 +1,47 @@
+import { ref, onMounted, onBeforeUnmount } from "vue"
+
+export const useDotButton = (emblaApi) => {
+  const selectedIndex = ref(0)
+  const scrollSnaps = ref([])
+
+  const onDotButtonClick = (index) => {
+    if (!emblaApi.value) return
+    emblaApi.value.scrollTo(index)
+  }
+
+  const onInit = (emblaApi) => {
+    scrollSnaps.value = emblaApi.value.scrollSnapList()
+  }
+
+  const onSelect = (emblaApi) => {
+    try {
+      selectedIndex.value = emblaApi.value.selectedScrollSnap()
+    } catch (error) {
+      selectedIndex.value = emblaApi.selectedScrollSnap()
+    }
+  }
+
+  onMounted(() => {
+    if (!emblaApi.value) return
+
+    onInit(emblaApi)
+    onSelect(emblaApi)
+    emblaApi.value.on('reInit', onInit)
+    emblaApi.value.on('reInit', onSelect)
+    emblaApi.value.on('select', onSelect)
+  })
+
+  onBeforeUnmount(() => {
+    if (!emblaApi.value) return
+
+    emblaApi.value.off('reInit', onInit)
+    emblaApi.value.off('reInit', onSelect)
+    emblaApi.value.off('select', onSelect)
+  })
+
+  return {
+    selectedIndex,
+    scrollSnaps,
+    onDotButtonClick
+  }
+}
